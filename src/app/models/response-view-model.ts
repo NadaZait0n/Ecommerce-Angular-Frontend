176e32ae24688f0681e523be36336758df7ffdba
eman mohamed <emanmohamed0901@gmail.com>
2023-03-15 12:27:40 +0200

    link login and register with api

---
+++ src/app/models/response-view-model.ts
@@ -0,0 +1,11 @@
+import { User } from "./user";
+
+export class ResponseViewModel{
+
+user:any
+success:any
+token:any
+message:any
+
+    
+}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from 'src/app/models/user';
+import { ResponseViewModel } from 'src/app/models/response-view-model';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  constructor(private _http: HttpClient, private _router: Router) { }
+  ngOnInit(): void {
+  }
+
+
+  signin(email: string, password: string) {
+    let user = new User();
+
+    user.email = email;
+
+    user.password = password;
+    this._http.post<ResponseViewModel>('http://localhost:8080/auth/login', user)
+      .subscribe(resposne => {
+        if (resposne.success) {
+          this._router.navigateByUrl('/home')
+        }
+        else
+          alert("this email registered before ");
+      })
+
+  }
 }
